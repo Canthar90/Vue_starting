1b80e34b62a5ca56ab7fe89d73393520da4e78d2
Canthar90 <d.cieslak90@gmail.com>
2023-06-01 22:43:17 +0200

    Added rest of the nav elements generated with v-for and added tests to it

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,14 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li v-for="item in menuItems" :key="item" class="ml-9 h-full first:ml-0">
+              <a href="#" class="flex h-full items-center py-2.5">{{ item }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -14,7 +21,9 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Myszojeleń Careers'
+      company: 'Myszojeleń Careers',
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Locations', 'Life at Bobo Corp', 'How we hire', 'Students', 'Jobs']
     }
   }
 }
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -8,4 +8,18 @@ describe('MainNav', () => {
     const companyName = screen.getByText('Myszojeleń Careers')
     expect(companyName).toBeInTheDocument()
   })
+
+  it('Displays menu items for navigations', () => {
+    render(MainNav)
+    const navigationMenuItems = screen.getAllByRole('listitem')
+    const navigatnonMenuTexts = navigationMenuItems.map((item) => item.textContent)
+    expect(navigatnonMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Bobo Corp',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
+  })
 })

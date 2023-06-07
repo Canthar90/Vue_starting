6f52189c5a998b563aba3fb34cb4411713ad6220
Canthar90 <d.cieslak90@gmail.com>
2023-06-07 12:21:44 +0200

    Added 2 way data binding 2 methods used

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -6,8 +6,10 @@
         <label class="absolute -top-10 left-0">Role</label>
         <input
           type="text"
+          :value="role"
           placeholder="Software engineer"
           class="w-full text-lg font-normal focus:outline-none"
+          @input="updateRole"
         />
       </div>
 
@@ -19,8 +21,10 @@
         <label class="absolute -top-10 left-0">Where?</label>
         <input
           type="text"
+          :value="location"
           placeholder="Los Angeles"
           class="w-full text-lg font-normal focus:outline-none"
+          @input="location = $event.target.value"
         />
       </div>
     </div>
@@ -34,6 +38,17 @@ import ActionButton from '@/components/ActionButton.vue'
 
 export default {
   name: 'JobSearchForm',
-  components: { ActionButton }
+  components: { ActionButton },
+  data() {
+    return {
+      role: '',
+      location: ''
+    }
+  },
+  methods: {
+    updateRole(event) {
+      this.role = event.target.value
+    }
+  }
 }
 </script>

f53bbdd3e751eb24c2e72a8cd6ace3e71f7bbb5a
Canthar90 <d.cieslak90@gmail.com>
2023-06-12 15:57:20 +0200

    Configuration of router added  variable

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
-<script setup></script>
-
 <template>
-  <div class="wrapper">
-    <main-nav />
-    <the-hero />
-  </div>
+  <main-nav />
+  <router-view />
 </template>
 
 <script>
 import MainNav from '@/components/Navigation/MainNav.vue'
-import TheHero from '@/components/JobSearch/TheHero.vue'
 
 export default {
   name: 'App',
-  components: {
-    MainNav,
-    TheHero
-  }
+  components: { MainNav }
 }
 </script>
--- src/views/JobsReasultsView.vue
+++ src/views/JobsReasultsView.vue
@@ -1,9 +1,16 @@
 <template>
   <h1 class="text-6xl font-bold">Jobs Reasults page</h1>
+  <h2>{{ experiment }}</h2>
 </template>
 
 <script>
 export default {
-  mame: 'JobsReasultsView'
+  mame: 'JobsReasultsView',
+  computed: {
+    experiment() {
+      console.log(this.$route)
+      return 'Any'
+    }
+  }
 }
 </script>

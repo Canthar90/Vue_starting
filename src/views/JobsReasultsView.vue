77b5f871e9ada8b79c57a7a5d0c33f529648bbca
Canthar90 <d.cieslak90@gmail.com>
2023-06-12 16:33:28 +0200

    Added routing to home page for ouer company name inside navbar

--- src/views/JobsReasultsView.vue
+++ src/views/JobsReasultsView.vue
@@ -1,15 +1,14 @@
 <template>
   <h1 class="text-6xl font-bold">Jobs Reasults page</h1>
-  <h2>{{ experiment }}</h2>
+  <button @click="goToHome">Go to Home Page</button>
 </template>
 
 <script>
 export default {
   mame: 'JobsReasultsView',
-  computed: {
-    experiment() {
-      console.log(this.$route)
-      return 'Any'
+  methods: {
+    goToHome() {
+      this.$router.push({ name: 'Home' })
     }
   }
 }

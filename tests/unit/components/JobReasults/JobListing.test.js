0f5fb8b1c7e63e2c4ab272dd14c466f77d9ccdd6
Canthar90 <d.cieslak90@gmail.com>
2023-06-15 11:17:16 +0200

    Added tests for Locations and qualifications

--- tests/unit/components/JobReasults/JobListing.test.js
+++ tests/unit/components/JobReasults/JobListing.test.js
@@ -4,7 +4,13 @@ import { RouterLinkStub } from '@vue/test-utils'
 import JobListing from '@/components/JobReasults/JobListing.vue'
 
 describe('JobListing', () => {
-  it('renders job title', () => {
+  const createJobProps = (jobProps = {}) => ({
+    title: 'Vue Developer',
+    organization: 'Any Corp',
+    ...jobProps
+  })
+
+  const renderJobListing = (jobProps) => {
     render(JobListing, {
       global: {
         stubs: {
@@ -13,28 +19,43 @@ describe('JobListing', () => {
       },
       props: {
         job: {
-          title: 'Vue Developer'
+          ...jobProps
         }
       }
     })
+  }
+
+  it('renders job title', () => {
+    const jobProps = createJobProps()
+    renderJobListing(jobProps)
 
     expect(screen.getByText('Vue Developer')).toBeInTheDocument()
   })
 
   it('renders job organization', () => {
-    render(JobListing, {
-      global: {
-        stubs: {
-          'router-link': RouterLinkStub
-        }
-      },
-      props: {
-        job: {
-          organization: 'Any Corp'
-        }
-      }
-    })
+    const jobProps = createJobProps()
+    renderJobListing(jobProps)
 
     expect(screen.getByText('Any Corp')).toBeInTheDocument()
+  })
+
+  it('renders job locations', () => {
+    const jobProps = createJobProps({
+      locations: ['Orlando', 'Torun']
+    })
+    renderJobListing(jobProps)
+
+    expect(screen.getByText('Orlando')).toBeInTheDocument()
+    expect(screen.getByText('Torun')).toBeInTheDocument()
+  })
+
+  it('renders job qualifications', () => {
+    const jobProps = createJobProps({
+      minimumQualifications: ['Hands', 'Licking elbow']
+    })
+    renderJobListing(jobProps)
+
+    expect(screen.getByText('Hands')).toBeInTheDocument()
+    expect(screen.getByText('Licking elbow')).toBeInTheDocument()
   })
 })

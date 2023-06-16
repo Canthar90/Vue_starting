1cd4249de4630a9ae27dd28392a806ec8b33ced3
Canthar90 <d.cieslak90@gmail.com>
2023-06-16 14:27:25 +0200

    Created isolated tests for pinia store user

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,5 +3,11 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('user', {
   state: () => ({
     isLoggedIn: false
-  })
+  }),
+  // user action is basicly method
+  actions: {
+    loginUser() {
+      this.isLoggedIn = true
+    }
+  }
 })

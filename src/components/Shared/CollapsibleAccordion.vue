c6163288eeb204ab0c304e78d9d8166b692498e2
Canthar90 <d.cieslak90@gmail.com>
2023-06-21 16:50:28 +0200

    Added basic tests for Collapsible Accordion and its slots

--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="border-b border-solid border-brand-gray-2 py-5 transition-all">
-    <div class="flex cursor-pointer flex-wrap items-center justify-between" @click="open">
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      @click="open"
+      role="button"
+    >
       <h3 class="text-base font-semibold">{{ header }}</h3>
       <font-awesome-icon :icon="caretIcon" />
     </div>

7c3e81cd3ff2586c7a5776360829dc35db00623d
Canthar90 <d.cieslak90@gmail.com>
2023-06-22 11:38:49 +0200

    Populating custom slots

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,15 @@
 <template>
-  <header-container></header-container>
+  <header-container>
+    <template v-slot:title>
+      <h1 class="w-full text-4xl font-normal">Tems</h1>
+    </template>
+
+    <template v-slot:subtitle>
+      <h2 class="my-4 w-full text-base font-light">
+        It's awesome working here. Why don't you come to join uss
+      </h2>
+    </template>
+  </header-container>
 </template>
 
 <script>

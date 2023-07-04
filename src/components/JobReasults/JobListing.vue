df4064a9f27fe2ff9c3b8a369608acf990c3abf3
Canthar90 <d.cieslak90@gmail.com>
2023-07-04 15:48:55 +0200

    Transformed JobListing template to composition api

--- src/components/JobReasults/JobListing.vue
+++ src/components/JobReasults/JobListing.vue
@@ -43,19 +43,15 @@
   </li>
 </template>
 
-<script>
-export default {
-  name: 'JobsListing',
-  props: {
-    job: {
-      type: Object,
-      required: true
-    }
-  },
-  computed: {
-    jobPageLink() {
-      return `/jobs/results/${this.job.id}`
-    }
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  job: {
+    type: Object,
+    required: true
   }
-}
+})
+
+const jobPageLink = computed(() => `/jobs/results/${props.job.id}`)
 </script>

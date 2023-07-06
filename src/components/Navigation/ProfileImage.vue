7d078a2a865b2b47be52f1b575a8b30560dd6653
Canthar90 <d.cieslak90@gmail.com>
2023-07-06 16:28:40 +0200

    Transforming ProfileImage component to compositionApi and TS

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -2,13 +2,11 @@
   <img :src="imageLink" alt="Profile pictiure" class="h-8 w-8 rounded-3xl object-contain" />
 </template>
 
-<script>
-export default {
-  name: 'ProfileImage',
-  data() {
-    return {
-      imageLink: 'https://play.nintendo.com/images/profile-zelda-link.7bf2a8f2.aead314d58b63e27.png'
-    }
-  }
-}
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const imageLink = ref(
+  'https://play.nintendo.com/images/profile-zelda-link.7bf2a8f2.aead314d58b63e27.png'
+)
+
 </script>

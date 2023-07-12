a7928a1cae9ae303e38790ddb744c25f08e00902
Canthar90 <d.cieslak90@gmail.com>
2023-07-12 11:26:40 +0200

    Created Prompt vue for clearing user job filters to clean code more

--- src/components/JobReasults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobReasults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -13,16 +13,7 @@
     }"
   >
     <section class="pb-5">
-      <div class="flex flex-row justify-between">
-        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
-        <div class="flex items-center text-sm">
-          <action-button
-            text="Clear Filters"
-            type="secondary"
-            @click="userStore.CLEAR_USER_JOB_FILTER_SELECTIONS"
-          />
-        </div>
-      </div>
+      <job-filters-sidebar-prompt />
 
       <collapsible-accordion header="Degrees">
         <job-filters-sidebar-degrees></job-filters-sidebar-degrees>
@@ -40,14 +31,13 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
-
 import ActionButton from '@/components/Shared/ActionButton.vue'
 import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
 
 import JobFiltersSidebarDegrees from '@/components/JobReasults/JobFiltersSidebar/JobFiltersSidebarDegrees.vue'
 import JobFiltersSidebarJobTypes from '@/components/JobReasults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue'
 import JobFiltersSidebarOrganizations from '@/components/JobReasults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue'
+import JobFiltersSidebarPrompt from '@/components/JobReasults/JobFiltersSidebar/JobFiltersSidebarPrompt.vue'
 
 import { useUserStore } from '@/stores/user'
 

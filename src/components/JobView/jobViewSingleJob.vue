32e7ecde5dad107b2f611705063a18f91a79b4b8
Canthar90 <d.cieslak90@gmail.com>
2023-07-19 13:42:28 +0200

    Improving structure

---
+++ src/components/JobView/jobViewSingleJob.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex-auto bg-brand-gray-2 p-8">
+    <!-- <div class="mx-24 block rounded border border-solid bg-white pt-8"> -->
+    <div :class="{ 'pt-24': !userStore.isLoggedIn }">
+      <div class="flex w-full flex-row justify-center text-center">
+        <div v-if="FILTERED_JOBS_BY_ID.length">
+          <SingleJobOffer v-for="job in FILTERED_JOBS_BY_ID" :key="job.id" :job="job" />
+        </div>
+        <div v-else>There is no job with this number</div>
+      </div>
+    </div>
+  </div>
+  <!-- </div> -->
+</template>
+
+<script lang="ts" setup>
+import { computed, onMounted, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { useUserStore } from '@/stores/user'
+import { useJobsStore } from '@/stores/jobs'
+import SingleJobOffer from '@/components/JobView/SingleJobOffer.vue'
+
+const route = useRoute()
+
+const userStore = useUserStore()
+const jobsStore = useJobsStore()
+onMounted(jobsStore.FETCH_JOBS)
+
+const currentJobId = computed(() => route.params.id as string)
+
+const relIdNumber = ref<number>(Number(currentJobId.value.split(':')[1]))
+userStore.UPDATE_ID(relIdNumber.value)
+
+const FILTERED_JOBS_BY_ID = computed(() => jobsStore.FILTERED_JOBS_BY_ID)
+
+watch([currentJobId], () => {
+  relIdNumber.value = Number(currentJobId.value.split(':')[1])
+  userStore.UPDATE_ID(relIdNumber.value)
+  const newSet = jobsStore.FILTERED_JOBS_BY_ID
+  return newSet
+})
+console.log(FILTERED_JOBS_BY_ID)
+</script>
--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,43 +1,7 @@
 <template>
-  <div class="flex-auto bg-brand-gray-2 p-8">
-    <!-- <div class="mx-24 block rounded border border-solid bg-white pt-8"> -->
-    <div :class="{ 'pt-24': !userStore.isLoggedIn }">
-      <div class="flex w-full flex-row justify-center text-center">
-        <div v-if="FILTERED_JOBS_BY_ID.length">
-          <SingleJob v-for="job in FILTERED_JOBS_BY_ID" :key="job.id" :job="job" />
-        </div>
-        <div v-else>There is no job with this number</div>
-      </div>
-    </div>
-  </div>
-  <!-- </div> -->
+  <jobViewSingleJob />
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
-import { useUserStore } from '@/stores/user'
-import { useJobsStore } from '@/stores/jobs'
-import SingleJob from '@/components/JobView/SingleJob.vue'
-
-const route = useRoute()
-
-const userStore = useUserStore()
-const jobsStore = useJobsStore()
-onMounted(jobsStore.FETCH_JOBS)
-
-const currentJobId = computed(() => route.params.id as string)
-
-const relIdNumber = ref<number>(Number(currentJobId.value.split(':')[1]))
-userStore.UPDATE_ID(relIdNumber.value)
-
-const FILTERED_JOBS_BY_ID = computed(() => jobsStore.FILTERED_JOBS_BY_ID)
-
-watch([currentJobId], () => {
-  relIdNumber.value = Number(currentJobId.value.split(':')[1])
-  userStore.UPDATE_ID(relIdNumber.value)
-  const newSet = jobsStore.FILTERED_JOBS_BY_ID
-  return newSet
-})
-console.log(FILTERED_JOBS_BY_ID)
+import jobViewSingleJob from '@/components/JobView/jobViewSingleJob.vue'
 </script>
